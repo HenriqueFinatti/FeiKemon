3cb6f82e6c0a7e7aa4517d2d716e261af127e185
Henrique Finatti <finatti.henrique@gmail.com>
2026-04-16 17:38:28 -0300

    Montando mapa da sala de estudos

---
+++ FEIKemon/assets/maps/tex_projects/bar_guss.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="bar_guss" tilewidth="16" tileheight="16" tilecount="1150" columns="46">
+ <image source="../sprites/bar_guss.png" width="736" height="400"/>
+</tileset>
